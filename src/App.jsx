7dfb42ee7aa97f285aba341d736e0909ee97078a
Mengkim21 <mengkim1221@gmail.com>
2025-05-28 21:35:54 +0700

    blue to purple

--- src/App.jsx
+++ src/App.jsx
@@ -28,8 +28,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
-      <h1 className="text-3xl font-bold text-cyan-500">
-        cyan
+      <h1 className="text-3xl font-bold text-purple-500">
+        purple
       </h1>
     </>
   )
