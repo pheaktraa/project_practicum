4ef73c7f001614812218855c6b72f0deb908d341
Pheaktra <13067888+pheaktraa@users.noreply.github.com>
2025-05-28 16:23:22 +0700

    tra change blue to green

--- src/App.jsx
+++ src/App.jsx
@@ -28,8 +28,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
-      <h1 className="text-3xl font-bold text-red-500">
-        red 
+      <h1 className="text-3xl font-bold text-green-500">
+        green 
       </h1>
     </>
   )
