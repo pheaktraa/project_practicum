84099e4766ab57871656cd9f720ff340f701883e
chinchannak <chinchannak625@gmail.com>
2025-05-28 14:30:05 +0700

    mac testing

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p> */}
       <h1 className="text-3xl font-bold text-green-500">
-        Tailwind is working correctly!
+        hiiiiii
       </h1>
     </>
   )
