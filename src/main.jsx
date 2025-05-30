68bc2d2dc4366b7ac6c3d92df2e331b1a4b6bacc
Pheaktra <13067888+pheaktraa@users.noreply.github.com>
2025-05-30 15:48:14 +0700

    initialize file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
-import App from './App.jsx'
+// import App from './App.jsx'
+import ContactUS from './pages/ContactUs';
+import AboutUs from './pages/AboutUs';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* <App /> */}
+      <ContactUS />
+      <AboutUs />
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,6 @@
+export default function AboutUs() 
+{
+    return (
+        <h1 className='text-green-500 text-3xl font-bold'>AboutUs</h1>
+    )
+}
