9a7f16a63a4501a3fb8d55aebcd55a3b4600af11
Pheaktra <130367888+pheaktraa@users.noreply.github.com>
2025-06-03 10:05:54 +0700

    install flowbite style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
+// tailwind.config.js
+import flowbite from 'flowbite/plugin';
+
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [flowbite],
+};
